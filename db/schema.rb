43bb4266c5a96d50700ef9357d0e5b1a50348643
=PhamKhanhDong <=khanhdong95vip@gmail.com>
2018-05-08 08:16:05 +0700

    chapter 9

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_085218) do
+
+ActiveRecord::Schema.define(version: 2018_05_13_071915) do
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 end
